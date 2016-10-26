2d61573f5c3e3924d9b839a29a7583df1a28007b
Joël Wasmer <morbrolhc@gmail.com>
2016-10-26 21:33:56 +0200

    First design

---
+++ src/li/wasmer/fxcalc/App.java
@@ -0,0 +1,24 @@
+package li.wasmer.fxcalc;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("res/fxCalc.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("fxCalc");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+}
